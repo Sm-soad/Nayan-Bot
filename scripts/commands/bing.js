6ef2a890aac66163e2f3f45a9cd15f5540f7aeb6
Sm soad <160392363+Sm-soad@users.noreply.github.com>
2024-04-27 13:18:17 +0600

    Add files via upload

--- scripts/commands/bing.js
+++ scripts/commands/bing.js
@@ -1,60 +1,82 @@
- module.exports = {
-  config: {
-    name: "bing",
-    version: "1.0.0",
-    permission: 0,
-    credits: "Nayan",
-    description: "",
-    prefix: 'awto',
-    category: "auto prefix",
-    usages: "bing prompt",
-    cooldowns: 10,
-},
-
-   languages: {
-   "vi": {},
-       "en": {
-           "missing": 'use : /bing cat'
-       }
-   },
-
-start: async function({ nayan, events, args, lang}) {
-    const axios = require("axios");
-    const fs = require("fs-extra");
-    const request = require("request");
-    const prompt = args.join(" ");
-    const key = this.config.credits;
-    const apis = await axios.get('https://raw.githubusercontent.com/MOHAMMAD-NAYAN/Nayan/main/api.json')
-  const n = apis.data.bing
-    if(!prompt) return nayan.reply(lang('missing'), events.threadID, events.messageID)
-
-  const rndm = ['cookie'] // input your cookie hare
-
-  var cookie = rndm[Math.floor(Math.random() * rndm.length)];
+module.exports.config = {
+  name: "video2",
+	version: "0.0.2",
+	permission: 0,
+  prefix: 'awto',
+	credits: "Nayan",
+	description: "Rendom video",
+	category: "user",
+	usages: "",
+    cooldowns: 5,
+};
 
 
-    const res = await axios.get(`${n}/bing-img?key=${key}&cookie=${cookie}&prompt=${encodeURIComponent(prompt)}`);
+module.exports🤩.run = async function({
+	event: e,
+	api: a,
+	args: n
+}) {
+	if (!n[0]) return a.sendMessage("====「 𝐕𝐈𝐃𝐄𝐎 」====\n━━━━━━━━━━━━━\n𝟙. 𝐋𝐎𝐕𝐄 𝐕𝐈𝐃𝐄𝐎 💞 \n𝟚. 𝐂𝐎𝐔𝐏𝐋𝐄 𝐕𝐈𝐃𝐄𝐎 💕\n𝟛. 𝐒𝐇𝐎𝐑𝐓 𝐕𝐈𝐃𝐄𝐎 📽\n𝟜. 𝐒𝐀𝐃 𝐕da𝐄𝐎 😔\n𝟝. 𝐒𝐓𝐀𝐓𝐔𝐒 𝐕𝐈𝐃𝐄𝐎 📝\n𝟞. 𝐒𝐇𝐀𝐈𝐑𝐈\n𝟟. 𝐁𝐀𝐁𝐘 𝐕𝐈𝐃𝐄𝐎 😻\n𝟠. 𝐀𝐍𝐈𝐌𝐄 𝐕𝐈𝐃𝐄𝐎 \n𝟡. 𝐇𝐔𝐌𝐀𝐈𝐘𝐔𝐍 𝐅𝐎𝐑𝐈𝐃 𝐒𝐈𝐑 ❄\n𝟙𝟘. 𝐈𝐒𝐋𝐀𝐌𝐈𝐊 𝐕𝐈𝐃𝐄𝐎 🤲\n\n===「 𝟏𝟖+ 𝐕𝐈𝐃𝐄𝐎 」===\n━━━━━━━━━━━━━\n𝟙𝟙. 𝐇𝐎𝐑𝐍𝐘 𝐕𝐈𝐃𝐄𝐎 🥵\n𝟙𝟚. 𝐇𝐎𝐓 🔞\n𝟙𝟛. 𝐈𝐓𝐄𝐌\n\nTell me how many video numbers you want to see by replaying this message", e.threadID, ((a, n) => {
+		global.client.handleReply.push({
+			name: this.config.name,
+			messageID: n.messageID,
+			author: e.senderID,
+      type: "create"
+		})
+	}), e.messageID)
+}, module.exports.handleReply = async ({
+	api: e,
+	event: a,
+	client: n,
+	handleReply: t,
+	Currencies: s,
+	Users: i,
+	Threads: o
+}) => {
+	var { p, h } = linkanh();
+  const request = require("request");
+	if ("create" === t.type) {
+		const n = (await p.get(h)).data.data;
+    const cap = (await p.get(h)).data.nayan;
+    const cn = (await p.get(h)).data.count;
+		let t = (await p.get(n, {
+			responseType: "stream"
+		})).data;
+		return e.sendMessage({
+			body: `${cap}` + `\n\n¤《𝐓𝐎𝐓𝐀𝐋 𝐕𝐈𝐃𝐄𝐎: ${cn}》¤`,
+			attachment: t
+		}, a.threadID, a.messageID)
+	}
 
-
-  console.log(res.data)
-    const data = res.data.result;
-  const numberSearch = data.length
-    var num = 0;
-    var imgData = [];
-    for (var i = 0; i < parseInt(numberSearch); i++) {
-      let path = __dirname + `/cache/${num+=1}.jpg`;
-      let getDown = (await axios.get(`${data[i]}`, { responseType: 'arraybuffer' })).data;
-      fs.writeFileSync(path, Buffer.from(getDown, 'utf-8'));
-      imgData.push(fs.createReadStream(__dirname + `/cache/${num}.jpg`));
-    }
-
-
-    nayan.reply({
-        attachment: imgData,
-        body: "🔍Bing Search Result🔍\n\n📝Prompt: " + prompt + "\n\n#️⃣Number of Images: " + numberSearch
-    }, events.threadID, events.messageID)
-    for (let ii = 1; ii < parseInt(numberSearch); ii++) {
-        fs.unlinkSync(__dirname + `/cache/${ii}.jpg`)
+    function linkanh() {
+        const p = require("axios");
+        const n = "http://65.109.58.118:26189";
+        if ("1" == a.body)
+            var h = `${n}/video/love`;
+        else if ("2" == a.body)
+         var   h = `${n}/video/cpl`;
+        else if ("3" == a.body)
+         var   h = `${n}/video/shortvideo`;
+        else if ("4" == a.body)
+          var  h = `${n}/video/sadvideo`;
+        else if ("5" == a.body)
+          var  h = `${n}/video/status`;
+        else if ("6" == a.body)
+          var  h = `${n}/video/shairi`;
+        else if ("7" == a.body)
+          var  h = `${n}/video/baby`;
+        else if ("8" == a.body)
+          var  h = `${n}/video/anime`;
+        else if ("9" == a.body)
+         var   h = `${n}/video/humaiyun`;
+        else if ("10" == a.body)
+         var  h = `${n}/video/islam`;
+        else if ("11" == a.body)
+         var  h = `${n}/video/horny`;
+        else if ("12" == a.body)
+          var  h = `${n}/video/hot`;
+        else if ("13" == a.body)
+          var  h = `${n}/video/item`;
+        return { p, h };
     }
 }
- }
